b55b328d2ec10312118fc691eb52d5c9710045a3
Olivier Chédru <ochedru@dhatim.com>
2019-04-17 12:41:15 +0200

    Update directoryController.js

--- src/api/controllers/directoryController.js
+++ src/api/controllers/directoryController.js
@@ -214,7 +214,7 @@ controller.consumeSamlAssertion = function (req, res) {
                         )
                     )
                         .then((idSite, token) => {
-                            logger('sso').debug('idSite url=%s', idSite.url);
+                            logger('sso').debug('idSite=%s, url=%s', JSON.stringify(idSite), idSite.url);
                                 const location = idSite.url + '/#/?jwt=' + token;
                                 logger('sso').debug('redirect to %s', location);
                                 return res.status(302).location(location).send();
